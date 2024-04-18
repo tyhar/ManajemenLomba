9d6a20ff2c2e58745884b06e25e81b6becc84029
taqideen <afamerteam@gmail.com>
2024-04-18 15:29:41 +0700

    set up required config from frontend
    - app.blade.php
    - public/
    - Welcome.vue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,94 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+
+        <!-- Dari Frontend -->
+            <link rel="icon" type="image/png" href="bootstrap/images/favicon.png">
+            <link rel="stylesheet" href="bootstrap/css/all.min.css">
+            <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+            <link rel="stylesheet" href="bootstrap/css/venobox.min.css">
+            <link rel="stylesheet" href="bootstrap/css/animated_barfiller.css">
+            <link rel="stylesheet" href="bootstrap/css/slick.css">
+            <link rel="stylesheet" href="bootstrap/css/animate.css">
+            <link rel="stylesheet" href="bootstrap/css/nice-select.css">
+            <link rel="stylesheet" href="bootstrap/css/spacing.css">
+            <link rel="stylesheet" href="bootstrap/css/style.css">
+            <link rel="stylesheet" href="bootstrap/css/responsive.css">
+            <!-- KE DUA -->
+            <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
+            <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+            <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+            <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+            <link href="assets/plugins/datatable/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+            <!-- loader-->
+            <link href="assets/css/pace.min.css" rel="stylesheet" />
+            <script src="assets/js/pace.min.js"></script>
+            <!-- Bootstrap CSS -->
+            <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet"> -->
+            <link href="assets/css/bootstrap-extendede.css" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+            <link href="assets/css/app.css" rel="stylesheet">
+            <link href="assets/css/icons.css" rel="stylesheet">
+            <!-- Theme Style CSS -->
+            <link rel="stylesheet" href="assets/css/dark-theme.css" />
+            <link rel="stylesheet" href="assets/css/semi-dark.css" />
+            <link rel="stylesheet" href="assets/css/header-colors.css" />
+            <!-- TAMBAHAN -->
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
     </head>
     <body class="font-sans antialiased">
+        {{-- file vue.js inertia --}}
         @inertia
+
+        {{-- Dari frontend --}}
+            <script src="bootstrap/js/jquery-3.6.3.min.js"></script>
+            <!--bootstrap js-->
+            <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+            <!--font-awesome js-->
+            <script src="bootstrap/js/Font-Awesome.js"></script>
+            <!--venobox js-->
+            <script src="bootstrap/js/venobox.min.js"></script>
+            <!--slick slider js-->
+            <script src="bootstrap/js/slick.min.js"></script>
+            <!--wow js-->
+            <script src="bootstrap/js/wow.min.js"></script>
+            <!--counterup js-->
+            <script src="bootstrap/js/jquery.waypoints.min.js"></script>
+            <script src="bootstrap/js/jquery.countup.min.js"></script>
+            <!--animated barfiller js-->
+            <script src="bootstrap/js/animated_barfiller.js"></script>
+            <!--sticky sidebar js-->
+            <script src="bootstrap/js/sticky_sidebar.js"></script>
+            <!--nice select js-->
+            <script src="bootstrap/js/jquery.nice-select.min.js"></script>
+            <!--main/custom js-->
+            <script src="bootstrap/js/main.js"></script>
+            <!-- KE DUA-->
+            <!-- Bootstrap JS -->
+            <script src="assets/js/bootstrap.bundle.min.js"></script>
+            <!--plugins -->
+            <!-- <script src="assets/js/jquery.min.js"></script> -->
+            <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
+            <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
+            <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+            <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+            <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+            <script src="assets/plugins/chartjs/js/Chart.min.js"></script>
+            <script src="assets/plugins/chartjs/js/Chart.extension.js"></script>
+            <!-- <script src="assets/js/index.js"></script> -->
+            <script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
+            <!--Morris JavaScript -->
+            <script src="assets/plugins/raphael/raphael-min.js"></script>
+            <script src="assets/plugins/morris/js/morris.js"></script>
+            <!-- <script src="assets/js/index2.js"></script> -->
+            <script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+            <script src="assets/plugins/datatable/js/dataTables.bootstrap5.min.js"></script>
+            <!--app JS-->
+            <script src="assets/js/app.js"></script>
+            <script src="admin/dist/libs/apexcharts/dist/apexcharts.min.js"></script>
+            <!-- Tabler Core -->
+            <script src="admin/dist/js/tabler.min.js"></script>
+
     </body>
 </html>
